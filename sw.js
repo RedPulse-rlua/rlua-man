b58384ece1ffaee34b7530897dd958ffdbf882bd
RedPulse-rlua <hydralauncher001@gmail.com>
2026-08-17 07:21:45 +0400

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'rlua-v1';
+const PRECACHE = ['/', '/manifest.json', '/favicon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((ks) =>
+      Promise.all(ks.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('/api/')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        if (res.ok && e.request.url.startsWith(self.location.origin)) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
